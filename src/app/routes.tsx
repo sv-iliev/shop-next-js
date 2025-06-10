6e056e09e0b1f06b0a44cfbc544944e5340c356f
Zak <opmyc@abv.bg>
2025-06-10 14:56:35 +0300

    implement navigation bar

---
+++ src/app/routes.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "@/app/pages/HomePage";
+import LoginPage from "@/app/pages/LoginPage";
+import ProductsPage from "@/app/pages/ProductsPage";
+import Cart from "@/app/pages/Cart";
+import Profile from "@/app/pages/Profile";
+import NotFoundPage from "@/app/pages/NotFoundPage";
+import { usePathname } from "next/navigation";
+import { LOGIN_ROUTE } from "@/app/helpers/constants";
+import styles from "@/app/styles/routes.module.scss";
+import Link from "next/link";
+
+const AppRouter: React.FC = () => {
+  const route: string = usePathname();
+  const isNotLoginRoute: boolean = route !== LOGIN_ROUTE;
+
+  return (
+    <Router>
+      {isNotLoginRoute && (
+        <div className={styles["nav-bar"]}>
+          <Link href="/">Home</Link>
+          <Link href="/products">Products</Link>
+          <Link href="/profile">Profile</Link>
+        </div>
+      )}
+
+      <div
+        className={
+          isNotLoginRoute ? styles["not-full-height"] : styles["full-height"]
+        }
+      >
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path={LOGIN_ROUTE} element={<LoginPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default AppRouter;
