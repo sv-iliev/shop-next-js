aa99bcca578a197a6a7cda29730701662338a371
Zak <opmyc@abv.bg>
2025-06-10 13:59:37 +0300

    implement NotFoundPage

--- src/app/pages/NotFoundPage.tsx
+++ src/app/pages/NotFoundPage.tsx
@@ -1,7 +1,23 @@
+"use client";
 import React from "react";
+import styles from "@/app/styles/NotFoundPage.module.scss";
+import { useRouter } from "next/navigation";
 
-type Props = {};
+export default function NotFoundPage(): React.JSX.Element {
+  const router = useRouter();
+  const { holder } = styles;
 
-export default function NotFoundPage({}: Props) {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className={holder}>
+      <h1>Page not found!</h1>
+      <button
+        onClick={() => {
+          router.push("/");
+        }}
+        className={styles.button}
+      >
+        Go to Home Page
+      </button>
+    </div>
+  );
 }
